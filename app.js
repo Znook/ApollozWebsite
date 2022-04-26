f35404d9e8acb1fa04054fccf7d90196ebf2591c
Jacob <Jacob.K289@outlook.com>
2022-04-26 19:06:03 +0200

    made the focus work and redid the js file

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const inputs = document.querySelectorAll(".input");
 
-function focusFunC(){
+function focusFunc() {
     let parent = this.parentNode;
     parent.classList.add("focus");
 }
 
-function blurFunC(){
-    let parent = this.parentNode;
-    if (this.value == ""){
-        parent.classList.remove("focus");
-    }
-}
-
-inputs.forEach(input => {
-    input.addEventListener("focus", focusFunC);
-    input.addEventListener("blur", blurFunC)
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc)
 });
